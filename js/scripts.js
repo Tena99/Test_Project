81326c766fa99c1542ed1ac722672a0a831514bb
Tena99 <strelka1515@gmail.com>
2022-08-05 13:19:35 +0300

    Homework 15. Redone.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,35 @@
-// Task 15.  Compact. Напишите функцию, которая очищает массив от нежелательных значений, таких как false, undefined, пустые строки, ноль, null.
+//Homework 15. Compact. Напишите функцию, которая очищает массив от нежелательных значений, таких как false, undefined, пустые строки, ноль, null.
 
-let result = "";
+//Option 1
 
-const compact = (array) => {
+function compact(array) {
+  let result = "";
 
-    if (typeof (array) == "number") {
-        return array;
+  array.filter(function _typeofFilter(item) {
+    let tmp = typeof item;
+    console.log(tmp, item);
+
+    if (
+      (tmp == "number" && item !== 0) ||
+      (tmp == "string" && item.length > 0)
+    ) {
+      result += `${item}, `;
     }
+  });
+
+  return result;
 }
 
-const data = [0, 1, false, 2, undefined, '', 3, null].filter(compact)
-console.log(data) // [1, 2, 3]
+const data = [0, 1, false, 2, undefined, "", 3, null, "a", "Hello"];
+console.log(compact(data)); // [1, 2, 3]
 
+// Option 2
 
-// -------------------------------------------------------------------------------------------//
+// function Compact(data) {
+//     return data.filter(function (item, index, array){
+//         return Boolean(item);
+//     });
 
-// function isBigEnough(value) {
-//
-//     return value >= 10;
-//
-// }
-//
-// let filtered = [12, 10, 2, 8, 130].filter(isBigEnough)
-// console.log(filtered)
-//
-// // -------------------------------------------------------------------------------------------//
-//
-// let isBigEnough = function (value) {
-//     return value >= 10;
-// }
-//
-// let filtered = [12, 10, 2, 8, 130].filter(isBigEnough)
-// console.log(filtered)
-//
-// // -------------------------------------------------------------------------------------------//
-//
-// let isBigEnough = (value) => {
-//     return value >= 10;
-// }
-//
-// let filtered = [12, 10, 2, 8, 130].filter(isBigEnough)
-// console.log(filtered)
+//  }
 
-// -------------------------------------------------------------------------------------------//
+//  alert(Compact([0, 1, false, 2, undefined, '', 3, null]));
